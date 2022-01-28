a32fb34ffcb6a584e714b27fd43c09fc9891d8b1
Jysan Aziz <jeexan2@gmail.com>
2022-01-29 04:55:26 +0600

    Overriding example

---
+++ samples/src/main/java/com/javacode/samples/overriding_methods/OverRidingMethods.java
@@ -0,0 +1,12 @@
+package com.javacode.samples.overriding_methods;
+
+import lombok.Data;
+
+@Data
+public class OverRidingMethods {
+
+    @Override
+    public String toString(){
+        return ("overriding");
+    }
+}
